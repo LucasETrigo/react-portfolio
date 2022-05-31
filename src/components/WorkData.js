36c78d3900351df5038196c092da16d99f83ff0b
Lucas Trigo <lucasetrigo@gmail.com>
2022-05-31 18:32:52 -0300

    React Portfolio

---
+++ src/components/WorkData.js
@@ -0,0 +1,40 @@
+export const Work = [
+    {
+        id: 1,
+        name: "Random Password Generator App",
+        description:
+            "It's a website that lets you generate a random password letting the user choose the lenth and a lot more options to make the password suit your needs.",
+        tags: ["JavaScript", "Html", "CSS"],
+
+        demo: "https://lucasetrigo.github.io/GeneradorClavesJS/",
+        github: "https://github.com/LucasETrigo/GeneradorClavesJS",
+    },
+    {
+        id: 2,
+        name: "NFTCall",
+        description:
+            "NFTCall is a website of all the latest news and information about upcoming NFT collections. It also has a premium membership area where users can purchase secret and more powerful NFT news before everyone.",
+        tags: ["Html", "sass"],
+
+        demo: "https://lucasetrigo.github.io/TheNFTCall/",
+        github: "https://github.com/LucasETrigo/SASS-NFTCall",
+    },
+    {
+        id: 3,
+        name: "Working On React Ecommerce WebSite",
+        description: "An NFT Marketplace made with React.js, upcoming...",
+        tags: ["react", "api", "sass"],
+
+        demo: "https://github.com/LucasETrigo/ecom-react-app",
+        github: "https://github.com/LucasETrigo/ecom-react-app",
+    },
+    {
+        id: 3,
+        name: "My Resume",
+        description: "Here you can take a look at my full resume.",
+        tags: ["Resume"],
+
+        demo: "https://drive.google.com/file/d/1eIg8t4lNECre5DIvix1CS6Rj6mRDoDXJ/view?usp=sharing",
+        github: "https://drive.google.com/file/d/1eIg8t4lNECre5DIvix1CS6Rj6mRDoDXJ/view?usp=sharing",
+    },
+];
